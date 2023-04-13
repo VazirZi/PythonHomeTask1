984ddcf6022feb7f28e94352f59e8cca7b398555
VazirZi <vazir.zivar@mail.ru>
2023-04-13 07:38:17 +0300

    add task 2

--- Unit1/Main.py
+++ Unit1/Main.py
@@ -9,3 +9,15 @@ while digit > 0:
     sum += digit % 10
     digit = int(digit / 10)
 print (sum)
+
+# Unit 2
+
+birdCount = int(input('Введите общее количество журавликов: '))
+part = birdCount / 3
+
+Katya = part * 2
+
+Petya = part / 2
+Sereja = Petya
+
+print(f"Катя сделала {int(Katya)} журавликов, Петя сделал {int(Petya)} журавликов, Сережа сделал {int(Sereja)} журавликов.")
